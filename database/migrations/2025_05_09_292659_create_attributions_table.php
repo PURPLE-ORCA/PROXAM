8ef7ac8490a13dfcd0a59381a5bd7b4844056f52
evarano <amiraamimri77@gmail.com>
2025-05-09 19:40:44 +0100

    correcte migration+ models

--- database/migrations/2025_05_09_292659_create_attributions_table.php
+++ database/migrations/2025_05_09_292659_create_attributions_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('attributions', function (Blueprint $table) {
-            Schema::create('attributions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('examen_id')->constrained('examens')->cascadeOnDelete();
             $table->foreignId('professeur_id')->constrained('professeurs')->cascadeOnDelete();
